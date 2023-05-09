395786151a6614aa469013ed1774fac96575e213
alexandr <svaluhinav@yandex.ru>
2023-05-09 20:58:37 +0500

    lab 15 task 3

---
+++ src/main/java/com/example/testrestservicetwo/TestRestServiceTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrestservicetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestServiceTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestServiceTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testrestservicetwo/TestRestServiceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrestservicetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestServiceTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
